4d4d5f09c2974fdd0e62bb9b3018405e1e7ddebc
wbglaeser <ben.glaeser@tuta.io>
2021-01-28 16:16:22 +0100

    add event overview

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 from app import db
 from models import Event
 
 main = Blueprint('main', __name__)
+pending_events = []
 
 @main.route('/')
 def index():
@@ -12,7 +15,10 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name)
+
+    events = Event.query.filter_by(user_id=current_user.id).all()
+
+    return render_template('profile.html', name=current_user.name, events=events)
 
 @main.route('/event')
 @login_required
@@ -25,12 +31,24 @@ def event_overview():
 
     # create new event
     new_event = Event(
+        user_id=current_user.id,
         occasion=request.form["occasion"],
         location=request.form["location"],
-        date=request.form["date"],
-        time=request.form["time"],
+        date=datetime.strptime(request.form["date"], '%Y-%m-%d').date(),
+        time=datetime.strptime(request.form["time"], '%M:%H').time(),
         subject=request.form["subject"],
         message=request.form["message"],
     )
-
+    pending_events.append(new_event)
+    
     return render_template('event_overview.html', event=new_event)
+
+@main.route('/event_confirmation', methods=['POST'])
+@login_required
+def event_confirmation():
+    
+    new_event = pending_events[0]
+    db.session.add(new_event)
+    db.session.commit()
+
+    return render_template('event_confirmation.html', event=new_event)
--- models.py
+++ models.py
@@ -2,13 +2,17 @@ from flask_login import UserMixin
 from database import db
 
 class User(UserMixin, db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    events = db.relationship("Event")
 
 class Event(UserMixin, db.Model):
+    __tablename__ = "event"
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     occasion = db.Column(db.String(100))
     location = db.Column(db.String(100))
     date = db.Column(db.Date)
